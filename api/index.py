cf4d16cb02141b2d28abe4c803bebb60f932e25b
ragsntatters <67745016+ragsntatters@users.noreply.github.com>
2025-01-17 00:49:45 +1300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from check_wordpress import check_wordpress
+from api.check_wordpress import check_wordpress
 import os
 from functools import wraps
 import time
